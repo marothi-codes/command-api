f57c330af23fc462a1083d79ae272e1407175320
Marothi Mahlake <code_artist@outlook.com>
2022-07-28 14:49:18 +0200

    Solution projects scaffolded.

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+
+namespace CommandAPI
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CommandsController: ControllerBase
+  {
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+      return new string[] { "this", "is", "a", "hard", "coded", "command" };
+    }
+  }
+}
