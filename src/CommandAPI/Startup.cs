f540a3b3ef06b7b6980c465e7f8572eb72ae65e0
Marothi Mahlake <code_artist@outlook.com>
2022-07-30 07:42:07 +0200

    DbContext configured & Db initial migration done.

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +28,11 @@ namespace CommandAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(
+                options =>
+                    options.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection"))
+            );
+
             services.AddControllers();
             services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
